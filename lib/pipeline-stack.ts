13d9483a1291b7371029ebcd265cb5244aa7e54e
oniblessingt <oniblessing.t@gmail.com>
2026-05-21 15:38:25 -0400

    Add CI/CD pipeline stack

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new codepipeline.Pipeline(this, 'AiCloudOpsPipeline', {
+      pipelineName: 'ai-cloudops-agent-pipeline',
+    });
+
+    new codebuild.Project(this, 'ValidationProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: ['npm install'],
+          },
+          build: {
+            commands: ['npm run build', 'npx cdk synth'],
+          },
+        },
+      }),
+    });
+  }
+}
